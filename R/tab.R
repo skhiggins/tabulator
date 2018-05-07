1b51d046dbd615cf5f0bfc03c649bd02002f57bf
skhiggins <seanhiggins@berkeley.edu>
2018-05-06 17:11:45 -0700

    Make tab() a generic; add tabcount(), quantiles()

--- R/tab.R
+++ R/tab.R
@@ -22,35 +22,37 @@
 #' b %>% tab(varname)
 #'
 #' @importFrom magrittr %>%
-#' @import dplyr
 #' @import data.table
-#' 
+#'
 #' @export
-tab <- function(df, ..., round=2){ # note ... is the variable names to group by
-  group_by <- dplyr::quos(...)
-  if ("data.table" %in% class(df)) { # to check without requiring data.table
-    group_by <- quo_to_chr(group_by)
-    rowsofdata <- df[, .N] # faster than nrow() on big data.tables
-    df[, .N, by = group_by][,
-      temp_prop := N/rowsofdata][,
-      prop := round(temp_prop, digits = round)][
-      order(-N)][, # sort in descending order by N before cumulative prop
-      cum_prop := round(cumsum(temp_prop), digits = round)][,
-      temp_prop := NULL][ # remove temp var
-      order(-N)] # make sure final data.table sorted
-  } else if ("tbl_df" %in% class(df)) { # to check without requiring tibble
-    rowsofdata <- nrow(df)
-    df %>%
-      group_by(!!!group_by) %>% # !!! since it's a quosure
-      summarize(N = n()) %>%
-      arrange(desc(N)) %>%
-      ungroup() %>%
-      mutate(
-        temp_prop = N/rowsofdata,
-        prop = round(temp_prop, digits = round),
-        cum_prop = round(cumsum(temp_prop), digits = round)
-      ) %>%
-      select(-temp_prop)
-  }
+tab <- function(df, ...) {
+  UseMethod("tab", df)
 }
 
+tab.data.table <- function(df, ..., round=2) { # note ... is the variable names to group by
+  group_by <- dplyr::quos(...) %>% quo_to_chr()
+  rowsofdata <- df[, .N] # faster than nrow() on big data.tables
+  df[, .N, by = group_by][,
+    temp_prop := N/rowsofdata][,
+    prop := round(temp_prop, digits = round)][
+    order(-N)][, # sort in descending order by N before cumulative prop
+    cum_prop := round(cumsum(temp_prop), digits = round)][,
+    temp_prop := NULL][ # remove temp var
+    order(-N)] # make sure final data.table sorted
+}
+
+tab.tbl_df <- function(df, ..., round = 2) { # to check without requiring tibble
+  group_by <- dplyr::quos(...)
+  rowsofdata <- nrow(df)
+  df %>%
+    dplyr::group_by(!!!group_by) %>% # !!! since it's a quosure
+    dplyr::summarize(N = n()) %>%
+    dplyr::arrange(desc(N)) %>%
+    dplyr::ungroup() %>%
+    dplyr::mutate(
+      temp_prop = N/rowsofdata,
+      prop = round(temp_prop, digits = round),
+      cum_prop = round(cumsum(temp_prop), digits = round)
+    ) %>%
+    dplyr::select(-temp_prop)
+}
