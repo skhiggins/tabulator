35f7a765e824d51865b5f54ad9ec1e94e351b568
skhiggins <seanhiggins@berkeley.edu>
2019-10-03 22:55:46 -0500

    purrr::map(rlang::asname)

--- R/tab.R
+++ R/tab.R
@@ -4,11 +4,11 @@
 #' \code{tab} shows the number of
 #' 	observations with that value, proportion of observations with that
 #' 		value, and cumulative proportion, in descending order of frequency.
-#' 		Accepts data.table or tibble as input.
+#' 		Accepts data.table, tibble, or data.frame as input.
 #' Efficient with big data: if you give it a \code{data.table},
 #' 		\code{tab} uses \code{data.table} syntax.
 #'
-#' @param df A data.table or tibble
+#' @param df A data.table, tibble, or data.frame
 #' @param ... A column or set of columns (without quotation marks)
 #' @param round An integer indicating the number of digits for proportion and cumulative proportion
 #' @return Tabulation (frequencies, proportion, cumulative proportion) for each unique value of the variables given in \code{...} from \code{df}.
@@ -21,6 +21,10 @@
 #' b <- tibble(varname = sample.int(20, size = 1000000, replace = TRUE))
 #' b %>% tab(varname)
 #'
+#' # data.frame
+#' c <- data.frame(varname = sample.int(20, size = 1000000, replace = TRUE))
+#' c %>% tab(varname)
+#'
 #' @importFrom magrittr %>%
 #' @import data.table
 #'
@@ -32,7 +36,7 @@ tab <- function(df, ...) {
 #' @export
 tab.data.table <- function(df, ..., round=2) { # note ... is the variable names to group by
   dt <- df # in case df has a condition on it
-  group_by <- rlang::enquos(...) %>% map(rlang::as_name) %>% unlist()
+  group_by <- rlang::enquos(...) %>% purrr::map(rlang::as_name) %>% unlist()
   rowsofdata <- dt[, .N] # faster than nrow() on big data.tables
   dt[, .N, by = group_by][,
     temp_prop := N/rowsofdata][,
